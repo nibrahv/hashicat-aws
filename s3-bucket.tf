23ddad37121fb1664d95f7829c90d7855ee1ea7b
John Harbin <jharbin@gmail.com>
2021-09-18 23:37:43 +0000

    Added s3 bucket module2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,8 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/jharbin-training/s3-bucket/aws"
+  version = "2.2.0"
+  # insert required variables here
 
-  # bucket = "my-s3-bucket"
   bucket_prefix = var.prefix
   acl    = "private"
 
